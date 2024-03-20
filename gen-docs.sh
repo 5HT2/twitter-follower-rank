6269446b4d6fc89132d3e10af0147888722bb7c2
5HT2 <git@frogg.ie>
2024-03-19 23:47:36 -0700

    gen-docs: Don't prefix code block with bash

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 USAGE="$(./twitter-follower-rank -help 2>&1 | gsed 's/\t/    /g' | gsed ':a;N;$!ba;s/\n/\\n/g' | gsed -E "s/\(/\\\(/g" | gsed -E "s/\)/\\\)/g")"
-perl -i -pe 'BEGIN{undef $/;} s/(<!--- GENERATED FROM MAKEFILE -->).*?(<!--- GENERATED FROM MAKEFILE -->)/$1\n```bash\nTWITTER_FOLLOWER_RANK_HELP\n```\n$2/smg' README.md
+perl -i -pe 'BEGIN{undef $/;} s/(<!--- GENERATED FROM MAKEFILE -->).*?(<!--- GENERATED FROM MAKEFILE -->)/$1\n```\nTWITTER_FOLLOWER_RANK_HELP\n```\n$2/smg' README.md
 gsed -i "s|TWITTER_FOLLOWER_RANK_HELP|${USAGE}|g" README.md
